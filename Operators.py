b565bee2a6b365e8361ea4741c35181df343c881
tazrianmon <kazitazrianmon@gmail.com>
2025-10-07 21:10:16 +0600

    Number Guessing Game

---
+++ Operators.py
@@ -0,0 +1,19 @@
+"""a= int(input())
+b= int(input())
+
+print(type(a))
+result = a+b
+
+print(result)"""
+
+
+"""x= float(input("Enter a float nm for x:"))
+y= int(input("Enter an integer nm for y :"))
+
+sum = x+y
+print(type(sum))
+print(sum)"""
+
+m = int(input())
+print(m/2)
+print(type(m))
---
+++ User_Input.py
@@ -0,0 +1,22 @@
+name = input()
+print ("My name is:", name)
+print("Mon")
+
+#Implicit (Automatic)
+
+a = 20.25 
+#print (type(a))
+a = "Ananta"
+print (type(a))
+
+#Explicit (manually declaring type)
+
+b = 13
+b = int(b)
+
+print (type(b))
+
+
+
+
+
